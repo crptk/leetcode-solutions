af023e648345592bcf2d8931999c947171cf03a6
Edrees <144494482+crptk@users.noreply.github.com>
2025-07-10 16:09:23 -0400

    Time: 0 ms (100%), Space: 18.3 MB (9.6%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -19,4 +19,14 @@ class Solution:
                 r = m
             else:
                 return nums[m]
-        
+
+'''
+In this solution using binary search, I checked specifically for if the value at the middle is over the value on the right pointer, if it is, that means that the minimum has to lie in that area, since the pivot would be there. If not, then the minimum is either the current middle or the leftmost side of the array. This covers all edge cases and is the most simple O(logn) solution I could think of. 
+
+Example:
+
+1. [4,5,6,7,0,1,2] | mid = 7 | Starting array
+2. [0,1,2] | mid = 1 | Cut off right left side since minimum is located on right side
+3. [0, 1] | mid = 0 | Cut off right side since minimum is located on left  side
+4. [0] | mid = 0 | Only one element left, which means m == r, which means it returns nums[m]
+'''
