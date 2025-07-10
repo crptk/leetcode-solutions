58b083c2fe31e450ad506a12012883f57ac2f474
Edrees <144494482+crptk@users.noreply.github.com>
2025-07-10 12:57:08 -0400

    Time: 0 ms (100%), Space: 18 MB (25.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,33 +1,26 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        Do not return anything, modify nums1 in-place instead.
+        Merges nums2 into nums1 in-place, assuming nums1 has enough space.
         """
-        nums3 = nums1[:m]
+        # Copy the first m elements of nums1
+        nums1_copy = nums1[:m]
 
-        # Pointers for each num
-        n1p, n2p, n3p = 0, 0, 0
-        iteration = 0
+        # Initialize pointers
+        p1 = p2 = p = 0
 
-        # Overrwriting elements until the end of one array is reached
-        while n2p < (len(nums2)) and n3p < (len(nums3)):
-            if nums3[n3p] < nums2[n2p]:
-                nums1[n1p] = nums3[n3p]
-                n3p += 1
-                n1p += 1
+        # Merge until one list is exhausted
+        while p1 < m and p2 < n:
+            if nums1_copy[p1] < nums2[p2]:
+                nums1[p] = nums1_copy[p1]
+                p1 += 1
             else:
-                nums1[n1p] = nums2[n2p]
-                n2p += 1
-                n1p += 1
-            iteration += 1
+                nums1[p] = nums2[p2]
+                p2 += 1
+            p += 1
 
-        # If there are any elements left in one of the arrays, append it to nums1
-        while n2p < len(nums2):
-            nums1[n1p] = nums2[n2p]
-            n2p += 1
-            n1p += 1
-        while n3p < len(nums3):
-            nums1[n1p] = nums3[n3p]
-            n3p += 1
-            n1p += 1
-        
+        # Append remaining elements
+        if p1 < m:
+            nums1[p:p + (m - p1)] = nums1_copy[p1:]
+        if p2 < n:
+            nums1[p:p + (n - p2)] = nums2[p2:]
