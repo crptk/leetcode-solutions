accad742c55a18ee30ccdec780a3cb6d359b2041
Edrees <144494482+crptk@users.noreply.github.com>
2025-07-10 12:49:03 -0400

    Time: 0 ms (100%), Space: 17.8 MB (71.84%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,18 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums3 = []
-        for i in range(m):
-            nums3.append(nums1[i])
+        nums3 = nums1[:m]
 
+        # Pointers for each num
         n1p, n2p, n3p = 0, 0, 0
         iteration = 0
 
-        print(f"length of nums2 - 1: {len(nums2) - 1} | length of nums3 - 1: {len(nums3) - 1}")
+        # Overrwriting elements until the end of one array is reached
         while n2p < (len(nums2)) and n3p < (len(nums3)):
-            print(f"Iteration: {iteration}")
-            print(f"n2p: {n2p}")
-            print(f"n3p: {n3p}")
             if nums3[n3p] < nums2[n2p]:
                 nums1[n1p] = nums3[n3p]
                 n3p += 1
@@ -25,7 +21,7 @@ class Solution:
                 n1p += 1
             iteration += 1
 
-            
+        # If there are any elements left in one of the arrays, append it to nums1
         while n2p < len(nums2):
             nums1[n1p] = nums2[n2p]
             n2p += 1
