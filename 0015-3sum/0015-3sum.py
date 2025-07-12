463f61c87f2382f9ef295d762308b1b5083ab8e6
Edrees <144494482+crptk@users.noreply.github.com>
2025-07-12 16:58:07 -0400

    Time: 519 ms (78.99%), Space: 20.6 MB (69.1%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -20,4 +20,13 @@ class Solution:
                         l += 1
         return res
 
-                    
+
+'''
+Solution by Edrees Amiri
+
+In this solution we are asked to find every set of three numbers in the array `nums` such that the sum of each set equals zero. To do this, we start by sorting the list since it’ll make it a lot easier to find duplicates. Then we iterate through the whole list using a for loop with a pointer `i` . 
+
+Inside the loop, we check if it’s a duplicate by checking the number before it, if it isn’t, then we grab a left pointer being the number after i, and a right pointer being the last value in the array. Afterwards, we check if the sum of all three numbers is zero, if it’s over zero, then we know that we should shift the right pointer down since it’s too high, if it’s less than zero, then we know that we should shift the left number upwards since it’s too low. Otherwise if it equals zero, then we found a valid set so we append it to our result list of sets.
+
+Now we need to shift our pointers forward. In order to do so with the least code, we shift our left pointer forwards until it’s unique and less than the right pointer.
+'''
