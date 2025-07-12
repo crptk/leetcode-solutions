c9439a2a6ecc2f8712fca62bf1bf4fa5c3fd2508
Edrees <144494482+crptk@users.noreply.github.com>
2025-07-12 15:19:40 -0400

    Time: 21 ms (98.47%), Space: 28.5 MB (67.23%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -5,12 +5,18 @@ class Solution:
         highest_area = 0
 
         while l != r:
+            hl, hr = height[l], height[r]
             distance = r - l # works
             area = min(height[r], height[l]) * distance
             highest_area = max(highest_area, area)
+
             if height[l] < height[r]:
                 l += 1
+                while l < r and height[l] <= hl:
+                    l += 1
             else:
                 r -= 1
+                while r > l and height[r] <= hr:
+                    r -= 1
 
         return highest_area
