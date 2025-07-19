a488c9df7915bba3021f8a6f1d96682aba5ebed8
Edrees <144494482+crptk@users.noreply.github.com>
2025-07-19 12:04:03 -0400

    Time: 20 ms (26.53%), Space: 31.6 MB (28.8%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -8,3 +8,9 @@ class Solution:
             distinct.add(num)
 
         return False
+
+'''
+Solution by Edrees
+
+This can be easily solved by creating a set, iterating through the list and checking if the number is present in the set. If it isn’t, then add that number to that set. This works because of the properties of a set. A set can only include distinct values and can retrieve values in O(1) time, which makes it the ideal method of solving this problem. Otherwise, you can check if the length of the list converted set is equal to the list, which has the same time/space complexity of O(n).
+'''
