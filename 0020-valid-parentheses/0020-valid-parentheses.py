65367ca8dbc82674fb073747106a129b5f17dead
Crptk <144494482+crptk@users.noreply.github.com>
2025-11-16 13:00:44 -0500

    Time: 0 ms (100%), Space: 17.9 MB (58.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,81 +1,30 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        stack = []
+        opening_characters = '({['
+        closing_characters = ')}]'
 
-        # stack = []
-
-        # mapping = {")" : "(", "}":"{", "]":"["}
-
-        # for char in s:
-        #     if char in mapping:
-        #         top_element = stack.pop() if stack else "#"
-
-        #         if mapping[char] != top_element:
-        #             return False
-            
-        #     else:
-        #         stack.append(char)
-
-        # return not stack
-
-        # Time Complexity: O(n)
-        # Space Complexity: O(n)
-
-
-                
-        # while "()" in s or "{}" in s or '[]' in s:
-        #     s = s.replace("()", "").replace('{}', "").replace('[]', "")
-        # return s == ''
-
-        # Time Complexity: O(n2)
-        # Space Complexity: O(n)
-
-
-
-        # openBracket = "({["
-        # closeBracket = ")}]"
-
-        # matching = {")":"(", "}":"{", "]":"["}
-
-        # stack = []
-        # for char in s:
-        #     if char in openBracket:
-        #         stack.append(char)
-        #     elif char in closeBracket:
-        #         if len(stack) == 0:
-        #             return False
-                
-        #         if stack[-1] == matching[char]:
-        #             stack.pop()
-        #         else:
-        #             return False
-        # return len(stack) == 0
-
-        # Time Complexity: O(n2)
-        # Space Complexity: O(n)
-
-
-        # stack = []
-        # closeToOpen = {")" : "(", "}" : "{", "]" : "["}
-
-        # for c in s:
-        #     if  c in closeToOpen:
-        #         if stack and stack[-1] == closeToOpen[c]:
-        #             stack.pop()
-                    
-        #         else:
-        #             return False
-        #     else:
-        #         stack.append(c)
-        # return True if not stack else False
-
-        sk = []
-        map_pa = {'(':')', '{':'}', '[':']'}
-
-        for c in s:
-            if c in map_pa:
-                sk.append(c)
-            elif sk and c == map_pa[sk[-1]]:
-                sk.pop()
+        bracket_dict = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+        for char in s:
+            if char in opening_characters:
+                stack.append(char)
+            elif char in closing_characters:
+                if not stack or bracket_dict[char] != stack[-1]:
+                    return False
+                stack.pop()
             else:
-                return False
-        return not sk
+                continue
+        return len(stack) == 0
+
+'''
+Solution by Crptk
+
+We basically keep storing the open brackets into a stack, and then as soon as we run into a closing
+bracket, we check if it corresponds to the top element of the stack (the last seen opening bracket) and 
+if it doesn't we return False. Keep doing this until we traverse the entire string, and return True if 
+the stack is empty, False if otherwise. This prevents unclosed open parantheses
+'''
