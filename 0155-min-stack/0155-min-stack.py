cdbdba71de10e4074f9be182029d12874b20da51
Crptk <144494482+crptk@users.noreply.github.com>
2025-11-19 14:58:08 -0500

    Time: 15 ms (12.75%), Space: 21.5 MB (35.95%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -17,3 +17,13 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minStack[-1]
+
+
+'''
+This solution uses a min stack and a stack to control
+the getMin function in O(1) time. Essentially, one stack
+is for the minimum, if it's less than the current minimum, then
+append it to the min stack. If not, then keep appending the current
+minimum (last val of minstack) onto the min stack so when
+we pop it, it won't mess with the order of the minstack.
+'''
