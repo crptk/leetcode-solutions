f6d4f18d5c98135d82533fb01cfd3dadea98051c
Crptk <144494482+crptk@users.noreply.github.com>
2026-02-20 12:43:01 -0500

    Time: 0 ms (100%), Space: 20.5 MB (33.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        hashmap = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[nums[i]] = i
+
         return False
